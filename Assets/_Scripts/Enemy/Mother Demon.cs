1d0d88821cb9c4cb0e2a70c7f7618255fc5f96c3
Alberto Tarabella <alberto.tarabella04@gmail.com>
2024-04-16 12:46:13 +0200

    final build 1.0

--- Assets/_Scripts/Enemy/Mother Demon.cs
+++ Assets/_Scripts/Enemy/Mother Demon.cs
@@ -6,14 +6,15 @@ public class MotherDemon : Enemy
 {
     [SerializeField] int numberOfChild;
 
+
     private void OnDisable()
     {
-        GameObject child = ObjectPooler.instance.GetPooledObject(4);
         for (int i = 0; i < numberOfChild; i++)
         {
+            GameObject child = ObjectPooler.instance.GetPooledObject(4);
             if (child != null)
             {
-                child.transform.position = transform.position;
+                child.transform.position = new Vector3(transform.position.x+numberOfChild*2, transform.position.y, transform.position.z);
                 child.SetActive(true);
             }
         }
